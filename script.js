8893bee9c8b621515930509e2eb153e673f76469
angie <angieangerilli@gmail.com>
2018-06-28 11:43:36 -0400

    second commit

---
+++ script.js
@@ -0,0 +1,63 @@
+
+//exercise one-------- 
+
+
+var num = prompt ("Please enter a number.");
+var total = 0;
+
+for (var i = num; i<= num; i++) {
+	total= total + i;
+}
+
+console.log (total);
+
+
+//exercise two-------------------------------------
+//run code first, then test if true, runs until false
+
+var play = prompt ("Would you like to play?");
+var word = prompt ("Please enter a word");
+
+do {
+	code to be executed
+} while (condition to test);//will run as long as condition is true
+
+
+
+
+//exercise three----------------------------------
+
+var answer = prompt ("Would you like to print your name?");
+var name = prompt ("What is your name?");
+var excl = ("!");
+
+while (answer = "yes"); {
+name + excl;
+excl ++;
+}
+
+alert ("My name is " + name + excl);
+
+
+
+
+//exercise four ------------------------------------
+var time = prompt ("Is it morning, noon or evening?");
+
+if (time === "morning") {
+	alert ("Since it is " + time + " you should be eating breakfast. We suggest eggs and toast.")
+
+} else if (time === "noon") {
+	alert ("Since it is " + time + " you should be eating lunch. We suggest a salad.")
+
+} else if (time === "evening") {
+	alert ("since it is " + time + " you should be eating dinner. We suggest chicken and rice.")
+
+} else {
+	alert ("Something went wrong.");
+} 
+
+
+
+
+
